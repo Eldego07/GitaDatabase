c958c42733a7a0f25cd1dabd8a9ad495182eddc6
Eldego07 <diego.casolaro8@gmail.com>
2026-03-30 10:20:44 +0200

    Initial commit

---
+++ src/gitadatabase/GitaDatabase.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gitadatabase;
+
+/**
+ *
+ * @author casolaro.diego
+ */
+public class GitaDatabase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
